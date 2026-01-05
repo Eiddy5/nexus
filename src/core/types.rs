522fc2444be049cdf66fe00b1b7027856b94d9a0
zhuangjianqi <zhuangjianqi@wehanyu.com>
2026-01-05 18:18:45 +0800

    fix:调整代码结构

--- src/core/types.rs
+++ src/core/types.rs
@@ -1,7 +1,6 @@
 use std::sync::Arc;
 use anyhow::Error;
 use async_trait::async_trait;
-use serde_json::Value;
 use crate::core::document::Document;
 
 #[async_trait]
@@ -15,10 +14,6 @@ pub trait Extension: Send + Sync {
 
     async fn on_authenticate(&self, _context: &HookContext) -> anyhow::Result<bool> {
         Ok(true)
-    }
-
-    async fn on_awareness_update(&self, _payload: &AwarenessPayload) -> anyhow::Result<()> {
-        Ok(())
     }
 
     async fn on_disconnect(&self, _context: &HookContext) -> anyhow::Result<()> {
@@ -41,18 +36,11 @@ pub trait Extension: Send + Sync {
     }
 }
 
-#[derive(Debug)]
-pub struct OnCreateDocumentPayload {}
-
-#[derive(Debug)]
-pub struct OnConnectionPayload {}
-
 #[derive(Clone)]
 pub struct OnLoadDocumentPayload {
     pub document:Arc<Document>
 }
 
-pub struct AfterLoadDocumentPayload {}
 
 #[derive(Debug, Clone)]
 pub struct ChangePayload {
@@ -74,17 +62,3 @@ pub struct HookContext {
     pub token: Option<String>,
 }
 
-#[derive(Clone, Debug)]
-pub struct StatelessPayload {
-    pub context: HookContext,
-    pub document_name: String,
-    pub payload: Value,
-}
-
-#[derive(Clone, Debug)]
-pub struct AwarenessPayload {
-    pub context: HookContext,
-    pub document_name: String,
-    pub client_id: u64,
-    pub state: Value,
-}
